50a4343bb98a3212f8d037ae6d856da14e53abcc
Wjimenezg <139941709+Wjimenezg@users.noreply.github.com>
2023-10-26 20:58:40 -0600

    Update ConsultaBD.java

--- ConsultaBD.java
+++ ConsultaBD.java
@@ -8,13 +8,17 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ConsultaBD {
-    //private ConexionBD conexionBD;
-            // Crea una instancia de la clase ConexionBD
-           ConexionBD conexionBD = new ConexionBD();
-        
-    
+    private ConexionBD conexionBD;
 
-    public void insertarUsuario(int IdProducto, String color ,int precio ,String material) {
+    public ConsultaBD() {
+        try {
+            conexionBD = new ConexionBD(); // Inicializa la conexión en el constructor
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void insertarUsuario(int IdProducto, String color ,double precio ,String material) {
         try {
             // Inserta un registro en la tabla usuarios
             Object[] datos = new Object[]{IdProducto, color, precio, material};
@@ -47,25 +51,61 @@ public class ConsultaBD {
     public void consultarUsuarios() {
         try {
             // Consulta todos los registros de la tabla usuarios
-            String sql = "SELECT * FROM usuarios";
+            String sql = "SELECT * FROM inventario";
             ResultSet resultados = conexionBD.consultar(sql);
 
             // Itera sobre los resultados
             while (resultados.next()) {
                 // Obtiene el valor de cada campo
-                int id = resultados.getInt("id");
-                String nombre = resultados.getString("nombre");
-                int edad = resultados.getInt("edad");
+                int IdProducto = resultados.getInt("IdProducto");
+                String tipoSilla = resultados.getString("tipoSilla");
+                int cantidad = resultados.getInt("cantidad");
 
                 // Imprime los valores
-                System.out.println("ID: " + id);
-                System.out.println("Nombre: " + nombre);
-                System.out.println("Edad: " + edad);
+                System.out.print("Producto: " + IdProducto +" ");
+                System.out.print("tipoSilla: " + tipoSilla +" ");
+                System.out.println("cantidad: " + cantidad );
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
+
+    public void consultarCantidad(int cantidadDeseada) {
+    try {
+        // Consulta los registros de la tabla Inventario que cumplen con la condición
+        String sql = "SELECT * FROM Inventario WHERE cantidad = ?";
+        
+        PreparedStatement statement = conexionBD.getConnection().prepareStatement(sql);
+        statement.setInt(1, cantidadDeseada);
+
+        ResultSet resultados = statement.executeQuery();
+
+        // Variable para rastrear si se encontró al menos un resultado
+        boolean encontrado = false;
+
+        // Itera sobre los resultados
+        while (resultados.next()) {
+            // Obtiene el valor de cada campo
+            int IdProducto = resultados.getInt("IdProducto");
+            String tipoSilla = resultados.getString("tipoSilla");
+            int cantidad = resultados.getInt("cantidad");
+
+            // Imprime los valores
+            System.out.print("Producto: " + IdProducto +" ");
+            System.out.print("tipoSilla: " + tipoSilla +" ");
+            System.out.println("cantidad: " + cantidad);
+            encontrado = true;
+        }
+
+        // Verifica si no se encontró ninguna cantidad y muestra un mensaje
+        if (!encontrado) {
+            System.out.println("No se encontró ninguna cantidad igual a " + cantidadDeseada + " en la base de datos.");
+        }
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+}
 
     public void cerrarConexion() {
         try {
